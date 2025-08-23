84d29b6c195d492e582774de1e568a2d38ddc168
Great Ezenna <ezennagreat@gmail.com>
2025-08-23 19:21:54 +0100

    socket now assigns a unique id to any connected user, and adds them to the client collection, key=id, value=socket connection

--- echo-server/main.js
+++ echo-server/main.js
@@ -5,15 +5,25 @@ const config = {
     port: 59300
 }
 
+const clients = Object.create(null);
+
 const server = net.createServer((socket) => {
+
+    // create a unique client id on connection
+    const clientID = Math.floor(Math.random() * 1000);
+
+    clients[clientID] = socket;
 
     console.log("Client connected!\n");
 
-    let messageCount = 0;
+    console.log(socket)
 
+
+    let messageCount = 0;
     socket.write("Say anything, and i'll echo it back!\n")
 
     socket.on("data", (data) => {
+
         messageCount++;
         let message = data.toString().trim();
 
