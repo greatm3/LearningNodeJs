f9a420f80eb8150103d8ee99f48516f33c54fae9
Great Ezenna <ezennagreat@gmail.com>
2025-08-24 12:40:51 +0100

    Created the broadcast message method in the server class, updated the handleConnection to notify other users, of a new user connection

--- echo-server/Server.js
+++ echo-server/Server.js
@@ -1,27 +1,34 @@
 import {User} from "./User.js";
-
-export const userPool = {};
+import net from "net";
 
 class Server {
     constructor (port, host) {
         this.port = port;
         this.host = host;
-
+        this.userPool = [];
     }
 
     start() {
-        // net.createServer((connection) => {
-        //     this.#handleConnection(connection);
-        // }).listen(this.port, this.host, () => {
-        //     process.stdout.write("Server started: " + this.host + ":" + this.port)
-        // })
-
-        this.#handleConnection("test");
+        net.createServer((connection) => {
+            this.#handleConnection(connection);
+        }).listen(this.port, this.host, () => {
+            process.stdout.write("Server started: " + this.host + ":" + this.port)
+        })
     }
 
     #handleConnection (connection) {
         this.client = User.createClient(connection);
-        userPool[this.client.id] = this.client;
+        this.broadcastMessage(`User ${this.client.id} connected\n`, this.client);
+        this.client.socket.write(`Welcome to the server ID: ${this.client.id} -> ${this.userPool.length} online\n`);
+        this.userPool.push(this.client);
+    }
+
+    broadcastMessage (message, sender) {
+        this.userPool.forEach((user) => {
+            if (user.socket !== sender.socket) {
+                user.socket.write(message);
+            }
+        })
     }
 
 }
