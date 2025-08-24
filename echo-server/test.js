f85f8d3dd3701ca97e5986328179a998c0af85d4
Great Ezenna <ezennagreat@gmail.com>
2025-08-24 12:41:25 +0100

    changed the start command to start a new seever intance

--- echo-server/test.js
+++ echo-server/test.js
@@ -1,11 +1,5 @@
-import Server, { userPool } from "./Server.js";
+import Server from "./Server.js";
 
 const server = new Server(59000, "localhost")
 
 server.start()
-server.start()
-server.start()
-
-console.log(server)
-console.log(server.client)
-console.log(userPool)
