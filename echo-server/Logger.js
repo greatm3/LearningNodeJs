9b517872f412d83f630baa47c96cc59f54e0c2a1
Great Ezenna <ezennagreat@gmail.com>
2025-08-24 14:16:26 +0100

    added methods, to create the folder and files if they don't exist, then listeners, that will call a private method, to log the messages, in the files

--- echo-server/Logger.js
+++ echo-server/Logger.js
@@ -1,11 +1,44 @@
 import path from "path";
+import fs from "fs";
 import Server from "./Server.js";
 
 class Logger {
     constructor({logPath, logFile, eventEmitter}) {
-        this.logPath = logPath;
-        this.logFile = logFile;
+        this.logPath = path.join(process.cwd(), logPath);
+        this.logFile = path.join(this.logPath, logFile);
         this.eventEmitter = eventEmitter;
+
+        this.#createPathIfNotExists(this.logPath);
+        this.#createFileIfNotExists(this.logFile);
+
+        this.eventEmitter.on("LOGERROR", error => this.#log(error, "ERROR", new Date().toISOString()))
+        this.eventEmitter.on("LOGINFO", info => this.#log(info, "INFO", new Date().toISOString()))
+    }
+
+    async #createPathIfNotExists(path) {
+        await fs.mkdir(path, (err) => {
+            if (err) {
+                throw err;
+            }
+        })
+    }
+
+    async #createFileIfNotExists(path) {
+        await fs.writeFile(path, "utf-8", (err) => {
+            if (err) {
+                throw err;
+            }
+        })
+    }
+
+    async #log(message, level, time) {
+        const logMessage = `[${time}] ${level}: ${message}`
+
+        await fs.appendFile(this.logFile, logMessage, err => {
+            if (err) {
+                throw err;
+            }
+        })
     }
 
     static start({ logPath, logFile, eventEmitter }) {
