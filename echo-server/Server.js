4817a0ead3463f6324d0511de98b2f84f03440e7
Great Ezenna <ezennagreat@gmail.com>
2025-08-28 13:40:05 +0100

    used prettier to format the script on save

--- echo-server/Server.js
+++ echo-server/Server.js
@@ -1,92 +1,92 @@
 import User from "./User.js";
 import net from "net";
-import {EventEmitter} from "events";
+import { EventEmitter } from "events";
 
 class Server extends EventEmitter {
-    constructor(port, host) {
-        super();
-        this.port = port;
-        this.host = host;
-        this.userPool = [];
-    }
+  constructor(port, host) {
+    super();
+    this.port = port;
+    this.host = host;
+    this.userPool = [];
+  }
 
-    start() {
-        const server = net.createServer((connection) => {
+  start() {
+    const server = net.createServer((connection) => {
 
-            const user = User.createClient(connection);
-            user.id = this.#assignID();
+      const user = User.createClient(connection);
+      user.id = this.#assignID();
 
-            this.#handleConnection(connection, user);
-            this.emit("LOGINFO", `new user connected -> ${user.id}`)
+      this.#handleConnection(connection, user);
+      this.emit("LOGINFO", `new user connected -> ${user.id}`)
 
-            connection.on("data", (data) => {
-                if (data.toString().trim() === "quit") {
-                    user.socket.end();
-                } else {
-                    this.broadcastMessage(`[${user.id}]: ${data.toString().trim()}\n`, user);
-                    user.messageCount++;
-                    this.emit("LOGINFO", `${user.id} -> new message: messageCount -> ${user.messageCount}`)
-                }
-            })
+      connection.on("data", (data) => {
+        if (data.toString().trim() === "quit") {
+          user.socket.end();
+        } else {
+          this.broadcastMessage(`[${user.id}]: ${data.toString().trim()}\n`, user);
+          user.messageCount++;
+          this.emit("LOGINFO", `${user.id} -> new message: messageCount -> ${user.messageCount}`)
+        }
+      })
 
-            connection.on("end", () => {
-                this.broadcastMessage(`[${user.id}] left the chat\n`, user);
-                this.emit("LOGINFO", `${user.id} exited the chat`)
-                this.userPool = this.userPool.filter((client) => {
-                    if (client.id !== user.id) {
-                        return client;
-                    }
-                })
-            })
-
-            connection.on("error", (err) => {
-                this.emit("LOGERROR", err.message);
-            })
-
+      connection.on("end", () => {
+        this.broadcastMessage(`[${user.id}] left the chat\n`, user);
+        this.emit("LOGINFO", `${user.id} exited the chat`)
+        this.userPool = this.userPool.filter((client) => {
+          if (client.id !== user.id) {
+            return client;
+          }
         })
+      })
 
-        server.listen(this.port, this.host, () => {
-            console.log("Server started: " + this.host + ":" + this.port);
-            this.emit("LOGINFO", `Server started: ${this.host}:${this.port}`);
-        })
+      connection.on("error", (err) => {
+        this.emit("LOGERROR", err.message);
+      })
 
-        // code to exit in case server shutdown mistakenly or using CTRL + C
-        process.on("SIGINT", async () => {
-            console.log("SHUTTING DOWN SERVER...")
+    })
 
-            server.close();
-            this.emit("LOGINFO", `Server closed: ${this.host}:${this.port}`);
+    server.listen(this.port, this.host, () => {
+      console.log("Server started: " + this.host + ":" + this.port);
+      this.emit("LOGINFO", `Server started: ${this.host}:${this.port}`);
+    })
 
-            this.userPool.forEach((user) => {
-                user.socket.write("Server is shutting down, GoodBye")
-                user.socket.end();
-            })
+    // code to exit in case server shutdown mistakenly or using CTRL + C
+    process.on("SIGINT", async () => {
+      console.log("SHUTTING DOWN SERVER...")
 
-            setTimeout(() => {
-                console.log("Server closed");
-                process.exit(0);
-            }, 500)
-        })
+      server.close();
+      this.emit("LOGINFO", `Server closed: ${this.host}:${this.port}`);
 
-    }
+      this.userPool.forEach((user) => {
+        user.socket.write("Server is shutting down, GoodBye")
+        user.socket.end();
+      })
 
-    #handleConnection(connection, user) {
-        this.broadcastMessage(`User ${user.id} connected\n`, user);
-        user.socket.write(`Welcome to the server ID: ${user.id} -> ${this.userPool.length} online\n`);
-        this.userPool.push(user);
-    }
+      setTimeout(() => {
+        console.log("Server closed");
+        process.exit(0);
+      }, 500)
+    })
 
-    #assignID() {
-        return Math.floor((Math.random() * 10000) + Date.now() / 5000);
-    }
+  }
 
-    broadcastMessage(message, sender) {
-        this.userPool.forEach((user) => {
-            if (user.socket !== sender.socket) {
-                user.socket.write(message);
-            }
-        })
-    }
+  #handleConnection(connection, user) {
+    this.broadcastMessage(`User ${user.id} connected\n`, user);
+    user.socket.write(`Welcome to the server ID: ${user.id} -> ${this.userPool.length} online\n`);
+    this.userPool.push(user);
+  }
+
+  #assignID() {
+    return Math.floor((Math.random() * 10000) + Date.now() / 5000);
+  }
+
+  broadcastMessage(message, sender) {
+    this.userPool.forEach((user) => {
+      if (user.socket !== sender.socket) {
+        user.socket.write(message);
+      }
+    })
+  }
 
 }
 
