b6842e600d5ff908bfb545655165360d2cb42e13
Great Ezenna <ezennagreat@gmail.com>
2025-08-28 16:09:27 +0100

    provided servername argument at Server instantiation

--- echo-server/main.js
+++ echo-server/main.js
@@ -1,12 +1,12 @@
 import Server from "./Server.js";
 import Logger from "./Logger.js";
 
-const server = new Server(59000, "localhost")
+const server = new Server(59000, "localhost", "Test Room")
 
 const loggerParameters = {
-    logFile: "log.txt",
-    logPath: "logs/",
-    eventEmitter: server
+  logFile: "log.txt",
+  logPath: "logs/",
+  eventEmitter: server
 }
 
 Logger.start(loggerParameters);
